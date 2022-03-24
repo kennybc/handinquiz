113309b870be5b73a9eb5a47372134081785f622
kennybc <54950614+kennybc@users.noreply.github.com>
2022-03-24 15:12:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const actions = require("./lib/actions.js");
 const sms = require("./lib/sms.js");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
